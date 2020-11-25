70cdb7c3339b9cc28df2c1325376c77af6c09c50
JOEUNJI <cling88@daum.net>
2020-11-25 17:06:24 +0900

    optimization using userReducer

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -5,7 +5,7 @@ import './TodoTemplate.scss';
 function TodoTemplate({ children }) {
     return (
         <div className="TodoTemplate">
-            <div className="app-title">일정관리222</div>
+            <div className="app-title">일정관리333</div>
             <div className="content">{children}</div>
         </div>
     )
