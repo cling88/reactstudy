ff02826ca70ebe90f9a57f3f26866b52a1700193
JOEUNJI <cling88@daum.net>
2020-11-25 17:37:29 +0900

    ss

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-
-import { useState,useRef, useCallback } from 'react';
+import { useReducer, useRef, useCallback } from 'react';
 
 import TodoTemplate from './components/TodoTemplate';
 // components
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos(){
+  const array =[];
+  for(let i = 1; i<= 2500; i++){
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false
+    })
+  }
+  return array; 
+}
+
+function todoReducer(todos, action) {
+  switch(action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id)
+    case 'TOGGLE':
+      return todos.map(todo => todo.id === action.id ? {...todo, checked: !todo.checked} : todo)
+    default:
+      return todos;
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'Study basic react',
-      checked: true
-    },
-    {
-      id: 2,
-      text: 'Styling components',
-      checked: true
-    },
-    {
-      id: 3,
-      text: 'Build Todo application',
-      checked: false
-    }
-  ])
+  
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos)
 
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(text => {
     const todo = {
@@ -34,19 +42,17 @@ function App() {
       text,
       checked: false
     }
-    setTodos(todos.concat(todo));
+    dispatch({type: 'INSERT', todo})
     nextId.current += 1;
-  }, [todos])
+  }, [])
 
   const onRemove = useCallback(id => {
-    todos.filter(todo => setTodos(todos.filter(todo => todo.id !== id)))
-  }, [todos])
+    dispatch({type: 'REMOVE', id})
+  }, [])
 
   const onToggle = useCallback(id => {
-    setTodos(
-      todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo)
-    )
-  }, [todos])
+    dispatch({type: 'TOGGLE', id})
+  }, [])
 
   return (
     <div className="App">
