653f219b426cb82340cb94c03e3457df0e14c1ad
cling88 <cling88@tlnine.co.kr>
2020-11-30 16:17:16 +0900

    route studying complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
-import { Route, Link } from 'react-router-dom'
+import React from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
 
 // components
 import Home from './components/Home'
@@ -19,11 +20,23 @@ function App() {
         {/* <li><Link to="/profile/user1" >user1 profile</Link></li>
         <li><Link to="/profile/user2" >user2 profile</Link></li> */}
       </ul>
-      <Route exact path="/" component={Home} />
-      <Route path={['/about', '/info']} component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/historySample" component={HistorySample} />
-      {/* <Route exact path="/profile/:username" component={Profile} /> */}
+      <hr/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path={['/about', '/info']} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route exact path="/historySample" component={HistorySample} />
+        <Route
+          render={({location}) => {
+            return(
+              <div>
+                <h2>이 페이지는 존재하지 않습니다</h2>
+                <p>{ location.pathname }</p>
+              </div>
+            )  
+          }}
+        />
+      </Switch>
     </div>
   );
 }
--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Link, Route, NavLink } from 'react-router-dom'
 import Profile from './Profile'
 
 
 
 function Profiles() {
+    const activeStyles = {
+        background: 'black',
+        color: 'white'
+    }
     return (
         <div>
             <h3>사용자 목록</h3>
             <ul>
-                <li><Link to="/profiles/user1">user1</Link></li>
-                <li><Link to="/profiles/user2">user2</Link></li>
+                <li><NavLink activeStyle={activeStyles} to="/profiles/user1">user1</NavLink></li>
+                <li><NavLink activeStyle={activeStyles} to="/profiles/user2">user2</NavLink></li>
             </ul>
             <Route path="/profiles" exact render={() => <div>사용자를 선택하세요</div>} />
             <Route path="/profiles/:username" component={Profile} />
