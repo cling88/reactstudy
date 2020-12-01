504818494f37187a3c2e043c56cda46f16de726e
cling88 <cling88@tlnine.co.kr>
2020-12-01 16:56:00 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -4,20 +4,6 @@ import { Route } from 'react-router-dom'
 import axios from 'axios';
 import NewsPage from './pages/NewsPage'
 
-// import NewsList from './components/NewsList'
-// import Categories from './components/Categories'
-// function App() {
-//   const [category, setCategory] = useState('all');
-//   const onSelect = useCallback(category => {
-//     setCategory(category)
-//   }, [])
-//   return (
-//     <div className="App">
-//       <Categories category={category} onSelect={onSelect}/>
-//       <NewsList category={category} />
-//     </div>
-//   );
-// }
 
 const App = () => {
   return <Route path="/:category?" component={NewsPage}/>
