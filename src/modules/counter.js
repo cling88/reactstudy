1098608234917bd9879bbbee9f39619e4255cc10
cling88 <cling88@tlnine.co.kr>
2020-12-07 11:33:42 +0900

    ff

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,30 +1,34 @@
+import { handleActions } from 'redux-actions'
 
-// actionTypes  // Ducks 패턴을 사용하는 대신 이름이 중복되지않게 모듈명을 넣어준다.
 const INCREASE = 'counter/INCREASE' 
 const DECREASE = 'counter/DECREASE'
 
 // actionFunctions
-export const increase = () => ({type: INCREASE });
-export const decrease = () => ({type: DECREASE });
+export const increase = handleActions(INCREASE)
+export const decrease = handleActions(DECREASE)
 
 const initialState = {
     number: 0
 }
 
-function counter( state= initialState, action ) {
+// function counter( state= initialState, action ) {
+//     switch(action.type) {
+//         case INCREASE: 
+//             return {
+//                 number: state.number + 1
+//             }
+//         case DECREASE: 
+//             return {
+//                 number: state.number - 1
+//             }
+//         default:
+//             return state
+//     }
+// }
 
-    switch(action.type) {
-        case INCREASE: 
-            return {
-                number: state.number + 1
-            }
-        case DECREASE: 
-            return {
-                number: state.number - 1
-            }
-        default:
-            return state
-    }
-}
+const counter = handleActions({
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+}, initialState)
 
 export default counter;
